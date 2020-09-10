fbd9868e0ef7a53e1f6db02c54f9c0e51ceeec9e
sourabh <sourabhchoudhary112112@gmail.com>
2020-09-10 19:39:04 +0530

    Optimizing page

--- src/main/home.js
+++ src/main/home.js
@@ -21,38 +21,44 @@ import 'aos/dist/aos.css';
 class Home extends React.Component {
 
 	  componentDidMount(){
-    AOS.init({offset: 500,duration: 3000,delay: 500})
+    AOS.init({duration: 3000})
   }
 
-	changeinfo = (ids,up,down) => {
-		var x= document.getElementById(ids);
-		var y = document.getElementsByClassName("info");
-		var z= document.getElementsByClassName("up");
-		var a= document.getElementsByClassName(up);
-		var b= document.getElementById(down);
-		var c= document.getElementsByClassName("down");
-		// for (var i = 0; i < y.length; i++) {
-		// 	y[i].style.display = "none";
-		// 	z[i].style.display="none";
-		// 	c[i].style.display="block";
-		// }
-		// x.style.display = "block";
-		// b.style.display="none";
-		// a[0].style.display = "block";
-		console.log("x")
-	}
+  constructor(props) {
+        super(props);
+        this.state ={
+            Stylesblock: [{display:'none'},{display:'none'},{display:'none'},],
+            Stylesicon1: [{display:'none'},{display:'none'},{display:'none'},],
+            Stylesicon: [{display:'block'},{display:'block'},{display:'block'},]
+        }
 
-	changeicon = (classes) => {
-		var x = document.getElementsByClassName(classes);
-		var y = document.getElementsByClassName("info");
-		var z = document.getElementsByClassName("down");
-		// for (var i = 0; i < y.length; i++) {
-		// 	y[i].style.display = "none";
-		// 	z[i].style.display="block";
-		// }
-		// x[0].style.display="none";
-		console.log(x);
-	}
+        this.baseState = this.state 
+    }
+
+
+  changeinfo (n) {
+  	var updating = [{display:'none'},{display:'none'},{display:'none'}];
+  	this.setState({Stylesblock: updating})
+  	updating[n] = {display:'block'}
+
+  	this.setState({Stylesblock: updating})
+
+  	var updating1 = [{display:'block'},{display:'block'},{display:'block'}];
+  	this.setState({Stylesicon: updating1})
+  	updating1[n] = {display:'none'}
+
+  	this.setState({Stylesicon: updating1})
+
+  	var updating2 = [{display:'none'},{display:'none'},{display:'none'}];
+  	this.setState({Stylesicon1: updating2})
+  	updating2[n] = {display:'block'}
+
+  	this.setState({Stylesicon1: updating2})
+  }
+
+  close () {
+  	this.setState(this.baseState)
+  }
 
 	render(){
    return ( 
@@ -76,8 +82,8 @@ class Home extends React.Component {
 		<h3 class="texthead">Book Store: </h3>
 		<p class="textdis">Buy and Sell books by Eliminating the middle men. Buy at Low cost, Sell at more price, Simply profitable.</p></div>
 		<p class="instruct">for more info click on arrow</p>
-		<p class="down" id="down1" onClick={this.changeinfo('info1','up1','down1')}><FaArrowDown /></p>
-		<p class="up1 up"  onClick={this.changeicon('up1')}><FaArrowUp /></p>
+		<p style={this.state.Stylesicon[0]} class="down" id="down1" onClick={() => this.changeinfo(0)}><FaArrowDown /></p>
+		<p style={this.state.Stylesicon1[0]} class="up1 up"  onClick={() => this.close()}><FaArrowUp /></p>
 	</div>
 	</div>
 	<div data-aos="fade-down" data-aos-duration="3000">
@@ -87,8 +93,8 @@ class Home extends React.Component {
 		<h3 class="texthead">Student Club: </h3>
 		<p class="textdis">Make your own club and connect with like minded people with same interests.</p></div>
 		<p class="instruct">for more info click on arrow</p>
-		<p class="down" id="down2" onClick={this.changeinfo('info2','up2','down2')}><FaArrowDown /></p>
-		<p class="up2 up" onClick={this.changeicon('up2')}><FaArrowUp /></p>
+		<p style={this.state.Stylesicon[1]} class="down" id="down2" onClick={() => this.changeinfo(1)}><FaArrowDown /></p>
+		<p style={this.state.Stylesicon1[1]} class="up2 up" onClick={() => this.close()}><FaArrowUp /></p>
 	</div>
 	</div>
 	<div data-aos="fade-down" data-aos-duration="3000">
@@ -98,49 +104,49 @@ class Home extends React.Component {
 		<h3 class="texthead">Course links: </h3>
 		<p class="textdis">Buy and Sell. Eliminate the middle men. Low cost, more profit.</p></div>
 		<p class="instruct">for more info click on arrow</p>
-		<p class="down" id="down3" onClick={this.changeinfo('info3','up3','down3')}><FaArrowDown /></p>
-		<p class="up3 up" onClick={this.changeicon('up3')}><FaArrowUp /></p>
+		<p style={this.state.Stylesicon[2]} class="down" id="down3" onClick={() => this.changeinfo(2)}><FaArrowDown /></p>
+		<p style={this.state.Stylesicon1[2]} class="up3 up" onClick={() => this.close()}><FaArrowUp /></p>
 	</div>
 	</div>
 </div>
 
-<div id="info1" class="info">
+<div style={this.state.Stylesblock[0]} id="info1" class="info">
 	Eliminate the middle man who racks up most of the profit when a book exchanges hands. Directly connect with the buyer or seller of the book thereby reducing the cost and increasing your profits.
 </div>
-<div id="info2" class="info">
+<div style={this.state.Stylesblock[1]} id="info2" class="info">
 	checking2
 </div>
-<div id="info3" class="info">
+<div style={this.state.Stylesblock[2]} id="info3" class="info">
 	checking3
 </div>
 <div class="extra-main">
 	<h1 class="extra-head">Other Services</h1>
 	<div class="grid1">
-	<p class="changelay1 lay" data-aos="fade-right" data-aos-duration="3000"><b class="extra-heading">Question Papers</b><br/>-every subject, every year.<br/>-from (insert year)<br/></p>
+	<p class="changelay1 lay" data-aos="fade-right" data-aos-duration="3000"><b class="extra-heading">Question Papers</b><br/>A catalogue of up to date question papers at your disposal.<br/><br/>•every subject, every year.<br/>•from (insert year)<br/></p>
 	<div class="extra-img" data-aos="fade-left" data-aos-duration="3000">
 	<img  src={ques} width="100%;" class ="extra" />
 	</div>
-	<p class="changelay2 lay" data-aos="fade-right" data-aos-duration="3000"><b  class="extra-heading">Question Papers</b><br/>-every subject, every year.<br/>-from (insert year)<br/></p>
+	<p class="changelay2 lay" data-aos="fade-right" data-aos-duration="3000"><b  class="extra-heading">Question Papers</b><br/>A catalogue of up to date question papers at your disposal.<br/><br/>•every subject, every year.<br/>•from (insert year)<br/></p>
 	<div class="extra-img" data-aos="fade-right" data-aos-duration="3000">
 	<img  src={event} width="100%;" class ="extra" />
 	</div>
-	<p class="lay" data-aos="fade-left" data-aos-duration="3000" ><b class="extra-heading">Event hub:</b><br/>-date and timing of every event<br/>-Name of the society hosting it<br/></p>
-	<p class="changelay1 lay" data-aos="fade-right" data-aos-duration="3000"><b class="extra-heading">Society hub:</b><br/>-information about every society<br/>-their forms and requirements to join<br/>-contact details</p>
+	<p class="lay" data-aos="fade-left" data-aos-duration="3000" ><b class="extra-heading">Event hub:</b><br/>Never miss an event ever again. One quick look at The Event Hub and you are good to go.<br/><br/>•date and timing of every event<br/>•Name of the society hosting it<br/>•Participation forms/requirements (if any)<br/></p>
+	<p class="changelay1 lay" data-aos="fade-right" data-aos-duration="3000"><b class="extra-heading">Society hub:</b><br/>Looking for a particular society or just want to explore your options? Our Society Hub has got you covered.<br/><br/>•information about every society<br/>•their forms and requirements to join<br/>•contact details</p>
 	<div class="extra-img" data-aos="fade-left" data-aos-duration="3000">
 	<img  src={society} width="100%;" class ="extra" />
 	</div>
-	<p class="changelay2 lay" data-aos="fade-right" data-aos-duration="3000"><b class="extra-heading">Society hub:</b><br/>-information about every society<br/>-their forms and requirements to join<br/>-contact details</p>
+	<p class="changelay2 lay" data-aos="fade-right" data-aos-duration="3000"><b class="extra-heading">Society hub:</b><br/>Looking for a particular society or just want to explore your options? Our Society Hub has got you covered.<br/><br/>•information about every society<br/>•their forms and requirements to join<br/>•contact details</p>
 	<div class="extra-img" data-aos="fade-right" data-aos-duration="3000">
 	<img  src={notes} width="100%;" class ="extra" />
 	</div>
-	<p class="lay" data-aos="fade-left" data-aos-duration="3000"><b class="extra-heading">Notes:</b><br/>-Get notes of the topics you missed or couldn't understand.<br/>-Notes of every subject available and for every year.<br/></p>
+	<p class="lay" data-aos="fade-left" data-aos-duration="3000"><b class="extra-heading">Notes:</b><br/>Simple and easy to understand notes for the perfect preparation. Never go unprepared ever again.<br/><br/>•Get notes of the topics you missed or couldn't understand.<br/>•Notes of every subject available and for every year.<br/></p>
 </div>
 </div>
 <div class="container con-add">
 <img src={background} class="her"/>
 <div class="request">
 	<h1 class="request-head">Join NSUT-HUB Support Team</h1>
-	<p class="request-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+	<p class="request-text">All of us know that getting hands on question papers, Notes ,E-Books is a difficult job and on top of it we are short-handed. We will appreciate any help we can get. You can contribute by sending us your notes, E-books, question papers. Small contributions from everyone will help us create the best collection of all the essentials. You can send the content on the Whatsapp group, join now by clicking the button given below.</p>
 </div>
 </div>
 
